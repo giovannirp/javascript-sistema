c21538ae6e4e09f48c65f19fa82d96cc4611dc3d
Giovanni Ribeiro Pinto <giovanni.ribeiro@grupoltm.com.br>
2020-03-04 16:46:52 -0300

    acertando javascript

--- pages/cadastro/cadastro-clientes.js
+++ pages/cadastro/cadastro-clientes.js
@@ -1,4 +1,5 @@
 const formCadastroCliente = document.querySelector('[data-form-clientes]');
+const nome = document.forms["formCadastro"]["nome"].value;
 
 formCadastroCliente.addEventListener("submit", event => {
     event.preventDefault();
@@ -6,10 +7,15 @@ formCadastroCliente.addEventListener("submit", event => {
     const nome = event.target.querySelector('[data-nome]').value;
     const cpf = event.target.querySelector('[data-cpf]').value;
 
+    if (validacaoForm) {
+        console.log("testandooooo")
+
+    }
+
     if (validaCPF(cpf)) {
         cadastrarClientes(nome, cpf);
     } else {
         alert("O CPF NÃO É VÁLIDO");
     }
 
-})
+});
